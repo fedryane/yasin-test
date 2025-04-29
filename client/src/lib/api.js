5a15d7e0ac240535291dcf1f79aa3587de260763
fedryane <bandarisafedryan@gmail.com>
2025-04-30 02:07:31 +0700

    ✨ FE : change API routes

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5001/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://yasin-test-server.vercel.app/api";
 
 // TOKEN MANAGEMENT
 
